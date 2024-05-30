0675348c607d3834d40c59236b0e73a3f45160e7
RezaReza27 <rezawendatour308@gmail.com>
2024-05-30 17:09:18 +0700

    Praktikum8

---
+++ vue-project-praktikum8/src/components/ArtikelHome.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <img> 
+        <h1>Ini adalah komponen artikel home</h1>
+        <p>Ini adalah isi artikel</p>
+        <p>Ini adalah isi artikel</p>
+        <p>Ini adalah isi artikel</p>
+    </div>
+</template>
